524f7a478a09ac8c23469257d0253c4bdd6c8014
Guryanov-Kirill <kg580652@gmail.com>
2026-04-29 12:05:25 +0300

    added implementations of functions in GameViewModel

--- src/main/kotlin/bohnanza/viewmodel/GameViewModel.kt
+++ src/main/kotlin/bohnanza/viewmodel/GameViewModel.kt
@@ -10,7 +10,24 @@ class GameViewModel {
     var currentTurn by mutableStateOf<Turn?>(null)
         private set
 
-    fun startGame() {
-        TODO()
+    fun startGame(playerNames: List<String>) {
+        val players = playerNames.mapIndexed { index, playerName -> Player(index, playerName) }
+        val newGame = Game(players, Deck())
+        newGame.startGame()
+        game = newGame
+        currentTurn = game?.currentTurn
+    }
+
+    fun nextTurn() {
+        game?.nextTurn()
+        currentTurn = game?.currentTurn
+    }
+
+    fun plantCard(fieldIndex: Int, card: Card, forced: Boolean) {
+        currentTurn?.plantCard(fieldIndex, card, forced)
+    }
+
+    fun harvestField(fieldIndex:Int): List<Card> {
+        return currentTurn?.harvestField(fieldIndex) ?: emptyList()
     }
 }
